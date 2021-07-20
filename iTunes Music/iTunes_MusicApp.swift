04183d297c8345c7089c9e1e390708128614016b
Ramy Sabry <ramysabry1996@gmail.com>
2021-07-20 15:28:37 +0200

    Initial Commit

---
+++ iTunes Music/iTunes_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunes_MusicApp.swift
+//  iTunes Music
+//
+//  Created by Ramy Sabry on 20/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct iTunes_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
